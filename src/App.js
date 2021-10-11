b5269b2b3df4024a4d47e2be7452220644e4c964
enPlace <n.place@protonmail.com>
2021-10-11 15:49:51 -0300

    Setup and connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { getFirestore } from "@firebase/firestore";
+import {
+  useFirebaseApp,
+  FirestoreProvider,
+  useUser,
+} from "reactfire";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import {
+  getAuth,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "@firebase/auth";
+
+const App = () => {
+  const firestoreInstance = getFirestore(useFirebaseApp());
+  const auth = getAuth(useFirebaseApp());
+  const { userStatus, data: user } = useUser();
+
+  const handleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch {}
+  };
+
+  if (userStatus === "loading") {
+    console.log(user);
+    return <span>loading...</span>;
+  }
+  if (!user) {
+    console.log(user);
+    return (
+      <div>
+        <button onClick={handleSignIn}>"Please sign in"</button>
+      </div>
+    );
+  } else
+    return (
+      <div>
+        {console.log(user.uid)}
+        <h1>Welcome Back, {user.displayName}!</h1>
+        <button
+          onClick={() => {
+            signOut(auth);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          sign out
+        </button>
+        <FirestoreProvider sdk={firestoreInstance}>
+          <h1>🌯</h1>
+    
+        </FirestoreProvider>
+      </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import { FirebaseAppProvider, AuthProvider, useFirebaseApp } from "reactfire";
+import { getAuth } from "@firebase/auth";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const UserAuth = () =>{
+  const auth = getAuth(useFirebaseApp());
+  return (
+    <AuthProvider sdk={auth}>
+       <App />
+    </AuthProvider>
+  )
+}
+
+render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+   <UserAuth></UserAuth>
+  </FirebaseAppProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
